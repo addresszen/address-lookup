07f50db863c31033359f4ac04ddc30d7ee9e37ca
Christopher Blanchard <git@nf3.co>
2022-04-04 21:35:29 +0200

    feat(USA): Resolve US formatted addresses

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { AddressFinder } from "@ideal-postcodes/address-finder";
 
 AddressFinder.defaults.baseUrl = "api.addresszen.com";
 AddressFinder.defaults.defaultCountry = "USA";
+AddressFinder.defaults.format = "usa";
 AddressFinder.defaults.titleizePostTown = false;
 
 export const AddressLookup = {
